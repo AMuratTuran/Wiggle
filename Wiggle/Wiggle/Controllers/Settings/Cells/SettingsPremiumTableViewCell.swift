263451e2a4fe4baf3a2c078b3e90107d66b9c0a7
Murat Turan <>
2019-11-23 13:29:15 +0300

    setting page init

---
+++ Wiggle/Wiggle/Controllers/Settings/Cells/SettingsPremiumTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsPremiumTableViewCell.swift
+//  Wiggle
+//
+//  Created by Murat Turan on 22.11.2019.
+//  Copyright © 2019 Murat Turan. All rights reserved.
+//
+
+import UIKit
+
+enum MembershipType: String {
+    case gold = "Wiggle Gold"
+    case plus = "Wiggle Plus"
+}
+
+class SettingsPremiumTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var rightFlame: UIImageView!
+    @IBOutlet weak var leftFlame: UIImageView!
+    @IBOutlet weak var containerView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    func prepare(type: MembershipType) {
+        switch type {
+        case .gold:
+            rightFlame.tintColor = UIColor.systemOrange
+            leftFlame.tintColor = UIColor.systemOrange
+        case .plus:
+            rightFlame.tintColor = UIColor.systemRed
+            leftFlame.tintColor = UIColor.systemRed
+        }
+    }
+    
+}
---
+++ Wiggle/Wiggle/Controllers/Settings/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  Wiggle
+//
+//  Created by Murat Turan on 22.11.2019.
+//  Copyright © 2019 Murat Turan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
