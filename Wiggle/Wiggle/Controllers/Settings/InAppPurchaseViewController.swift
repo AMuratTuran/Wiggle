af248de7372572babb6b8a8d817719ecfe51d3dc
Tolga <mtas13@ku.edu.tr>
2020-01-20 00:59:59 +0300

    iap screen added

---
+++ Wiggle/Wiggle/Controllers/Settings/InAppPurchaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InAppPurchaseViewController.swift
+//  Wiggle
+//
+//  Created by MUSTAFA TOLGA TAS on 20.01.2020.
+//  Copyright © 2020 Murat Turan. All rights reserved.
+//
+
+import UIKit
+
+class InAppPurchaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
